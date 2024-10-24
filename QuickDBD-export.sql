eb6a5702f4e535d0e8dde6c2ca969c90c3311ac5
Aaqib7007 <aaqibjabbarhj@gmail.com>
2024-10-23 19:50:33 -0500

    additional files

---
+++ QuickDBD-export.sql
@@ -0,0 +1,13 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "sodt-attributes" (
+    "rater_id" INT   NOT NULL,
+    "reference_title" VARCHAR(255)   NOT NULL,
+    "soft_attribute" VARCHAR(255)   NOT NULL,
+    "less_than" VARCHAR(255)   NOT NULL,
+    "about_as" VARCHAR(255)   NOT NULL,
+    "more_than" VARCHAR(255)   NOT NULL
+);
+
